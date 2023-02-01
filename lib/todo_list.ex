84b81a4a418e2b11c32453dd37666a82a20089fd
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2023-02-01 19:48:10 -0300

    feat: add update entry

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,11 +1,45 @@
 defmodule TodoList do
-  def new(), do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
+
+  def new(), do: %TodoList{}
 
   def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
+
+    %TodoList{
+      todo_list
+      | entries: new_entries,
+        auto_id: todo_list.auto_id + 1
+    }
   end
 
   def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+    todo_list.entries
+    |> Stream.filter(fn {_id, entry} -> entry.date == date end)
+    |> Enum.map(fn {_id, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
   end
 end
