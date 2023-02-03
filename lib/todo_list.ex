ee89453e4d5fbe6630a0180f4755a5230fb9eff4
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2023-02-03 16:02:43 -0300

    feat: csvimporter

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -3,6 +3,14 @@ defmodule TodoList do
 
   def new(), do: %TodoList{}
 
+  def new_entries(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
+
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
 
@@ -41,5 +49,55 @@ defmodule TodoList do
 
   def update_entry(todo_list, %{} = new_entry) do
     update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
+  end
+end
+
+defmodule TodoList.CsvImporter do
+  def import(filename) do
+    filename
+    |> read_lines()
+    |> create_entries()
+    |> TodoList.new_entries()
+  end
+
+  defp read_lines(file_name) do
+    file_name
+    |> File.stream!()
+    |> Stream.map(&String.replace(&1, "\n", ""))
+  end
+
+  defp create_entries(lines) do
+    lines
+    |> Stream.map(&extract_fields/1)
+    |> Stream.map(&create_entry/1)
+  end
+
+  defp extract_fields(line) do
+    line
+    |> String.split(",")
+    |> convert_date
+  end
+
+  defp convert_date([date_string, title]) do
+    {parse_date(date_string), title}
+  end
+
+  defp parse_date(date) do
+    date
+    |> String.split("/")
+    |> Enum.map(&String.to_integer/1)
+    |> then(&create_date/1)
+  end
+
+  defp create_date([year, month, day]) do
+    Date.new!(year, month, day)
+  end
+
+  defp create_entry({date, title}) do
+    %{date: date, title: title}
   end
 end
